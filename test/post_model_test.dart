3a56d97319a37463d7e6f57d69f1cdb7d398a995
Alexander Avramenko <a.avramenko@kryptonite.ru>
2021-05-13 21:54:14 +0300

    Add tests for OutgoingPostModel

--- test/post_model_test.dart
+++ test/post_model_test.dart
@@ -10,6 +10,7 @@ void main() {
   const String TEXT = 'some text';
   const int CREATED = 1234567;
   DateTime created = DateTime.fromMillisecondsSinceEpoch(CREATED * 1000);
+  const PostStatus STATUS = PostStatus.SENT;
 
   test('PostModel() constructor', () {
     var model = PostModel(
@@ -45,5 +46,45 @@ void main() {
     expect(model.userId, 0);
     expect(model.text, '');
     expect(model.created, DateTime.fromMillisecondsSinceEpoch(0));
+  });
+
+  test('OutgoingPostModel() constructor', () {
+    var model = OutgoingPostModel(
+        chatId: CHAT_ID, userId: USER_ID, text: TEXT, status: STATUS);
+
+    expect(model.id, 0);
+    expect(model.chatId, CHAT_ID);
+    expect(model.userId, USER_ID);
+    expect(model.text, TEXT);
+    expect(model.status, STATUS);
+  });
+
+  test('OutgoingPostModel.from(model) constructor', () {
+    var model = OutgoingPostModel.from(
+        PostModel.from(Post(
+            chatId: Int64(CHAT_ID),
+            userId: Int64(USER_ID),
+            created: Int64(CREATED),
+            text: TEXT,
+            id: Int64(ID))),
+        STATUS);
+
+    expect(model.id, ID);
+    expect(model.chatId, CHAT_ID);
+    expect(model.userId, USER_ID);
+    expect(model.text, TEXT);
+    expect(model.created, created);
+    expect(model.status, STATUS);
+  });
+
+  test('OutgoingPostModel must be constructable from empty post model', () {
+    var model = OutgoingPostModel.from(PostModel.from(Post()), STATUS);
+
+    expect(model.id, 0);
+    expect(model.chatId, 0);
+    expect(model.userId, 0);
+    expect(model.text, '');
+    expect(model.created, DateTime.fromMillisecondsSinceEpoch(0));
+    expect(model.status, STATUS);
   });
 }
